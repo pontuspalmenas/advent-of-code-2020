1ba86c27a9b36a5eda05704f789675392fd25eff
Pontus Palmenäs <pontus@palmenas.se>
2020-12-06 22:51:11 +0100

    Added comment

--- day01/main.go
+++ day01/main.go
@@ -11,6 +11,8 @@ func main() {
 	fmt.Println(Solve2(input))
 }
 
+// Todo: unoptimized. Solve for O(n), O(n^2) for p1, p2
+
 func Solve1(input []int) int {
 	for _, i := range input {
 		for _, j := range input {
