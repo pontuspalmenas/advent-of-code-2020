3fc66c767273d28029c93db307f0ad968907f33a
Pontus Palmenäs <pontus.palmenas@seb.se>
2020-12-22 09:52:51 +0100

    day17 p1&p2

--- day17/debug.go
+++ day17/debug.go
@@ -5,12 +5,12 @@ import (
 	"fmt"
 )
 
-func (u *Universe3) print(size int) {
+func (u *Universe4) print(size int) {
 	for z := -1; z < 2; z++ {
 		Printfln("z=%d",z)
 		for y:=-size; y<size+size; y++ {
 			for x:=-size; x<size+size; x++ {
-				if u.active(P3{z:z, y:y, x:x}) {
+				if u.active(P4{z: z, y:y, x:x}) {
 					fmt.Print("#")
 				} else {
 					fmt.Print(".")
@@ -21,10 +21,10 @@ func (u *Universe3) print(size int) {
 	}
 }
 
-func (u *Universe3) debug(p P3) {
+func (u *Universe4) debug(p P4) {
 	Printfln("{%d,%d,%d} %v", p.z, p.y, p.x, u.active(p))
 }
 
-func (u *Universe3) debugActiveNeighs(p P3) {
+func (u *Universe4) debugActiveNeighs(p P4) {
 	Printfln("{%d,%d,%d} %d", p.z, p.y, p.x, u.activeNeighbors(p))
 }
--- day17/main.go
+++ day17/main.go
@@ -16,12 +16,22 @@ func main() {
 	fmt.Println("p2:", p2)
 }
 
+// todo: want a generic PointN with dim []int to solve both problems, but can't use slice as map key...
+
 type P3 struct {
 	z, y, x int
 }
 
+type P4 struct {
+	z, y, x, w int
+}
+
 type Universe3 struct {
 	state map[P3]bool
+}
+
+type Universe4 struct {
+	state map[P4]bool
 }
 
 func NewUniverse3() *Universe3 {
@@ -29,15 +39,22 @@ func NewUniverse3() *Universe3 {
 	return &Universe3{state}
 }
 
-func (u *Universe3) set(p P3) {
-	u.state[p] = true
-}
-
-func (u *Universe3) unset(p P3) {
-	u.state[p] = false
+func NewUniverse4() *Universe4 {
+	state := make(map[P4]bool)
+	return &Universe4{state}
 }
 
 func (u *Universe3) count() int {
+	count := 0
+	for _, active := range u.state {
+		if active {
+			count++
+		}
+	}
+	return count
+}
+
+func (u *Universe4) count() int {
 	count := 0
 	for _, active := range u.state {
 		if active {
@@ -53,7 +70,7 @@ func (u *Universe3) activeNeighbors(p P3) int {
 		for y := p.y-1; y <= p.y+1; y++ {
 			for x := p.x-1; x <= p.x+1; x++ {
 				np := P3{z: z, y: y, x: x}
-				if samePoint(p, np) {
+				if p == np {
 					continue
 				}
 				if u.active(np) {
@@ -65,7 +82,31 @@ func (u *Universe3) activeNeighbors(p P3) int {
 	return count
 }
 
+func (u *Universe4) activeNeighbors(p P4) int {
+	count := 0
+	for z := p.z-1; z <= p.z+1; z++ {
+		for y := p.y-1; y <= p.y+1; y++ {
+			for x := p.x-1; x <= p.x+1; x++ {
+				for w := p.w-1; w <= p.w+1; w++ {
+					np := P4{z: z, y: y, x: x, w: w}
+					if p == np {
+						continue
+					}
+					if u.active(np) {
+						count++
+					}
+				}
+			}
+		}
+	}
+	return count
+}
+
 func (u *Universe3) active(p P3) bool {
+	return u.state[p]
+}
+
+func (u *Universe4) active(p P4) bool {
 	return u.state[p]
 }
 
@@ -74,11 +115,7 @@ func Solve1(input []string) int {
 	// init
 	for i, row := range input {
 		for j, col := range row {
-			if col == '#' {
-				u.set(P3{y: i, x: j, z: 0})
-			} else {
-				u.unset(P3{y: i, x: j, z: 0})
-			}
+			u.state[P3{y: i, x: j}] = col == '#'
 		}
 	}
 
@@ -118,12 +155,49 @@ func (u *Universe3) NewState() map[P3]bool {
 	return newState
 }
 
-func Solve2(input []string) int {
-	return 0
+func (u *Universe4) NewState() map[P4]bool {
+	newState := make(map[P4]bool)
+	for k, v := range u.state {
+		newState[k] = v
+	}
+	return newState
 }
 
-func samePoint(p1 P3, p2 P3) bool {
-	return p1.z == p2.z &&
-		p1.y == p2.y &&
-		p1.x == p2.x
+func Solve2(input []string) int {
+	u := NewUniverse4()
+	// init
+	for i, row := range input {
+		for j, col := range row {
+			u.state[P4{y: i, x: j}] = col == '#'
+		}
+	}
+
+	// starting size
+	size := len(input[0])
+
+	for gen := 1; gen <= 6; gen++ {
+		newState := u.NewState()
+		for z := -gen; z <= gen; z++ {
+			for y := -gen; y < size+gen; y++ {
+				for x := -gen; x < size+gen; x++ {
+					for w := -gen; w < size+gen; w++ {
+						p := P4{z: z, y: y, x: x, w: w}
+						activeNs := u.activeNeighbors(p)
+						if u.active(p) {
+							if !(activeNs == 2 || activeNs == 3) {
+								newState[p] = false
+							}
+						} else {
+							if activeNs == 3 {
+								newState[p] = true
+							}
+						}
+					}
+				}
+			}
+		}
+		u.state = newState
+	}
+
+	return u.count()
 }
