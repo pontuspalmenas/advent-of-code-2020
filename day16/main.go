d28e443331d961ae05d70399b302cce064d72950
Pontus Palmenäs <pontus.palmenas@seb.se>
2021-01-05 14:24:50 +0100

    day16 messy

--- day16/main.go
+++ day16/main.go
@@ -4,6 +4,7 @@ import (
 	. "aoc"
 	. "aoc/types"
 	"fmt"
+	"strings"
 	"time"
 )
 
@@ -47,6 +48,7 @@ func Solve1(input string) int {
 func Solve2(input string) int {
 	split := SplitByEmptyNewline(input)
 	rules := Lines(split[0])
+	myTicket := Ints(Lines(split[1])[1])
 	nearby := tickets(Lines(split[2])[1:])
 	var fields []Field
 
@@ -57,40 +59,55 @@ func Solve2(input string) int {
 	tickets, _ := sortTickets(nearby, fields)
 
 	println(len(tickets))
-	//mapped := make(map[string]int)
-	var unmapped []string
+	mapped := make(map[string]int)
 
 	// find candidates
+	candidates := findCandidates(fields, tickets)
+
+	// reduce
+	for len(mapped) < len(fields) {
+		for i, c := range candidates {
+			if c.Size() == 1 {
+				name := c.ToSlice()[0]
+				mapped[name] = i
+				for _, c := range candidates {
+					c.Remove(name)
+				}
+			}
+		}
+	}
+
+	factor := 1
+	for k, v := range mapped {
+		if strings.HasPrefix(k, "departure") {
+			factor *= myTicket[v]
+		}
+	}
+
+	return factor
+}
+
+func findCandidates(fields []Field, tickets []Ticket) map[int]*StringSet {
 	candidates := make(map[int]*StringSet)
-	for i := 0; i <= len(tickets[0])-1; i++ {
+	for i := 0; i <= len(fields)-1; i++ {
 		candidates[i] = NewStringSet()
 	}
 
-	for _, t := range tickets {
-		for i, v := range t {
-			candidates[i] = candidates[i].Union(candidatesForIndex(fields, v))
+	for i := 0; i <= len(fields)-1; i++ {
+		for _, f := range fields {
+			allValid := true
+			for _, t := range tickets {
+				if !inRange(f, t[i]) {
+					allValid = false
+					break
+				}
+			}
+			if allValid {
+				candidates[i].Add(f.name)
+			}
 		}
 	}
-	// debug
-	for k, v := range candidates {
-		Printfln("%d -> %v", k, v)
-	}
-
-	for _, f := range fields {
-		unmapped = append(unmapped, f.name)
-	}
-
-	return 0
-}
-
-func candidatesForIndex(fields []Field, value int) *StringSet {
-	set := NewStringSet()
-	for _, f := range fields {
-		if inRange(f, value) {
-			set.Add(f.name)
-		}
-	}
-	return set
+	return candidates
 }
 
 func sortTickets(tickets []Ticket, fields []Field) (valid []Ticket, sumInvalid int) {
