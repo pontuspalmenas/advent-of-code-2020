ab070ec6464e16f6a7c5616d4b3d026c841d565f
Pontus Palmenäs <pontus@palmenas.se>
2021-01-24 22:29:53 +0100

    day22 p2 wip

--- day22/main.go
+++ day22/main.go
@@ -22,7 +22,7 @@ type Deck struct {
 	cards []int
 }
 
-func (d *Deck) Top() int {
+func (d *Deck) Peek() int {
 	return d.cards[0]
 }
 
@@ -46,7 +46,7 @@ func (d *Deck) String() string {
 }
 
 func Draw(p1 *Deck, p2 *Deck) *Deck {
-	if p1.Top() > p2.Top() {
+	if p1.Peek() > p2.Peek() {
 		return p1
 	}
 	return p2
@@ -61,26 +61,20 @@ func (d *Deck) Score() int {
 	return score
 }
 
+func (d *Deck) CopySubset(n int) *Deck {
+	return &Deck{player: d.player, cards: d.cards[:n]}
+}
+
 func Solve1(input string) int {
 	p1, p2 := ParseInput(input)
 
 	var winner *Deck
 	round := 1
 	for p1.Size() > 0 && p2.Size() > 0 {
-		Printfln("-- Round %d --", round)
-		Printfln(p1.String())
-		Printfln(p2.String())
-		Printfln("Player 1 plays: %d", p1.Top())
-		Printfln("Player 2 plays: %d", p2.Top())
 		winner = Draw(p1, p2)
-		Printfln("Player %d wins the round!", winner.player)
 		winner.Add(p1.Pop(), p2.Pop())
 		round++
 	}
-
-	Printfln("== Post-game results ==")
-	Printfln(p1.String())
-	Printfln(p2.String())
 
 	if p1.Size() == 0 {
 		return p2.Score()
@@ -89,7 +83,65 @@ func Solve1(input string) int {
 }
 
 func Solve2(input string) int {
-	return 0
+	p1, p2 := ParseInput(input)
+
+	// Before either player deals a card, if there was a previous round in this game that had exactly the same cards
+	// in the same order in the same players' decks, the game instantly ends in a win for player 1.
+
+	// Otherwise, this round's cards must be in a new configuration;
+	// the players begin the round by each drawing the top card of their deck as normal.
+
+	// If both players have at least as many cards remaining in their deck as the value of the card they just drew,
+	// the winner of the round is determined by playing a new game of Recursive Combat
+
+	// Otherwise, at least one player must not have enough cards left in their deck to recurse;
+	// the winner of the round is the player with the higher-value card.
+
+	// To play a sub-game of Recursive Combat, each player creates a new deck by making a copy of the next cards in their deck
+	// (the quantity of cards copied is equal to the number on the card they drew to trigger the sub-game).
+
+	winner := RecursiveCombat(1, p1, p2, []Deck{})
+
+	if p1.Size() == 0 {
+		return p2.Score()
+	}
+	return p1.Score()
+}
+
+func RecursiveCombat(game int, p1, p2 *Deck, history []Deck) *Deck {
+	// todo: check if previous round had exact same cards
+	if playedBefore(p1, history) || playedBefore(p2, history) {
+		return p1
+	}
+
+	c1 := p1.Pop()
+	c2 := p2.Pop()
+	if c1 == p1.Size() && c2 == p2.Size() {
+		return RecursiveCombat(game+1, p1.CopySubset(c1), p2.CopySubset(c2), history)
+	} else {
+		return Draw(p1, p2)
+	}
+}
+
+func playedBefore(d *Deck, history []Deck) bool {
+	for _, h := range history {
+		if equal(d.cards, h.cards) {
+			return true
+		}
+	}
+	return false
+}
+
+func equal(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
 }
 
 func ParseInput(input string) (*Deck, *Deck) {
--- day22/main_test.go
+++ day22/main_test.go
@@ -26,7 +26,19 @@ Player 2:
 
 func Test2(t *testing.T) {
 	s:=
-``
+`Player 1:
+9
+2
+6
+3
+1
 
-	AssertEq(t, Solve2(s), 0)
+Player 2:
+5
+8
+4
+7
+10`
+
+	AssertEq(t, Solve2(s), 291)
 }
